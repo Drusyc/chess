1ef866bf61be3ad4330968b56aba2febcfb7cb66
utilisateur <brazousd@telesun.imag.fr>
2014-11-17 15:39:51 +0100

    model

---
+++ src/model/Case.java
@@ -0,0 +1,48 @@
+package model;
+
+/**
+ * Created by amadou on 17/11/14.
+ */
+public class Case {
+    private int x;
+    private int y;
+    private boolean occupee;
+    
+    	
+    
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public boolean isOccupee() {
+		return occupee;
+	}
+    
+    
+  
+    
+    
+    /*
+    private ArrayList<Observateur> listeObservateurs = new ArrayList<Observateur>();
+
+
+    @Override
+    public void attach(Observateur obs) {
+        this.listeObservateurs.add(obs);
+    }
+
+    @Override
+    public void detach() {
+        //Suppression de tous les observateurs
+        this.listeObservateurs = new ArrayList<Observateur>();
+    }
+
+    @Override
+    public void inform() {
+        for (Observateur obs : this.listeObservateurs) {
+            obs.mAj();
+        }
+    }*/
+}
---
+++ src/model/Deplacement.java
@@ -0,0 +1,57 @@
+package model;
+
+import pieces.Cavalier;
+import pieces.Fou;
+import pieces.Piece;
+import pieces.Pion;
+import pieces.Reine;
+import pieces.Roi;
+import pieces.Tour;
+
+
+public class Deplacement implements Visiteur {
+    
+	Case dest;
+    
+    
+    public void deplace (Piece p){
+    	
+    }
+    
+	@Override
+	public void visite(Pion p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visite(Tour p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visite(Fou p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visite(Reine p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visite(Roi p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visite(Cavalier p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
