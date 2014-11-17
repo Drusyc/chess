c1ad8d1817426bbcc7c1abf888c62e48068fa7ef
ahasall <ahasall@yahoo.fr>
2014-11-17 02:57:42 +0100

    Interversion Observable/Observateur

--- src/observer/Observable.java
+++ src/observer/Observable.java
@@ -4,5 +4,8 @@ package observer;
  * Created by amadou on 17/11/14.
  */
 public interface Observable {
-    public void mAj();
+
+    public void attach(Observateur obs);
+    public void detach();
+    public void inform();
 }
--- src/observer/Observateur.java
+++ src/observer/Observateur.java
@@ -4,7 +4,5 @@ package observer;
  * Created by amadou on 17/11/14.
  */
 public interface Observateur {
-    public void attach(Observateur obs);
-    public void detach();
-    public void inform();
+    public void mAj();
 }
