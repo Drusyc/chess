f8232ba938863b24e1b1cf39a3c5cc783bd45385
utilisateur <brazousd@telesun.imag.fr>
2014-11-17 14:55:53 +0100

    model

--- src/pieces/Cavalier.java
+++ src/pieces/Cavalier.java
@@ -1,7 +1,15 @@
 package pieces;
 
+import model.Visiteur;
+
 /**
  * Created by amadou on 17/11/14.
  */
 public class Cavalier extends Piece {
+
+	@Override
+	void appliquer(Visiteur v) {
+		v.visite(this);
+		
+	}
 }
--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,7 +1,10 @@
 package pieces;
 
+import model.Visiteur;
 /**
  * Created by amadou on 17/11/14.
  */
-public class Piece {
+public abstract class Piece {
+	
+	abstract void appliquer (Visiteur v);
 }
